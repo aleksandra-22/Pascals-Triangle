6d12254fbe6832ab9e889120b66c029b633001c5
Aleksandra <aleksandra.katic99@gmail.com>
2021-07-12 13:35:00 +0200

    Add new methods: calculate, draw and to_s, make changes to main file, lets the user pick number of rows, change triangle method to triangle_row

--- lib/main.rb
+++ lib/main.rb
@@ -1,3 +1,11 @@
 require_relative "pascals_triangle"
 
-(5).times { |rows| p PascalsTriangle.new().triangle(rows) }
+begin
+    print "How many rows would you like the triangle to have? "
+    input = Integer(gets.chomp)
+rescue
+    puts "Please enter a whole number(an Integer)."
+    retry
+end
+
+PascalsTriangle.new().draw(input)
--- lib/pascals_triangle.rb
+++ lib/pascals_triangle.rb
@@ -1,20 +1,28 @@
 class PascalsTriangle
-    
     def initialize
         @triangle = [[1]]
     end
 
-    def triangle(row_num)
-        if @triangle[row_num]
-            return @triangle[row_num]
-        else
-            old_row = triangle(row_num - 1)
-            middle = []
-            (old_row.length - 1).times do |i|
-                middle.push(old_row[i] + old_row[i + 1])
-            end
-            rows = [1, middle, 1].flatten # da spoji nizove tj da ne bi bilo npr [1, [2], 1] nego [1, 2, 1]
+    def triangle_row(row_num)
+        return @triangle[row_num] if @triangle[row_num]
+        old_row = triangle_row(row_num - 1)
+        @triangle[row_num] = calculate(old_row)
+    end
+
+    def calculate(old_row)
+        middle = []
+        (old_row.length - 1).times do |i|
+            middle.push(old_row[i] + old_row[i + 1])
         end
+        rows = [1, middle, 1].flatten
         rows
+    end
+
+    def draw(input)
+        (input).times { |rows| p PascalsTriangle.new.to_s(rows) }
+    end
+
+    def to_s(row_num)
+        triangle_row(row_num).join(" ").center(40)
     end
 end

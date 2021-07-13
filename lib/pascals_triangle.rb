49629fe923afacb56f700203ae243cafd7ce7c3a
Aleksandra <aleksandra.katic99@gmail.com>
2021-07-13 11:29:56 +0200

    Change method name, create a new constant, remove unnecessary line

--- lib/pascals_triangle.rb
+++ lib/pascals_triangle.rb
@@ -1,28 +1,29 @@
 class PascalsTriangle
+    ALIGN_CENTER = 40
+
     def initialize
         @triangle = [[1]]
     end
 
     def triangle_row(row_num)
         return @triangle[row_num] if @triangle[row_num]
-        old_row = triangle_row(row_num - 1)
-        @triangle[row_num] = calculate(old_row)
+        previous_row = triangle_row(row_num - 1)
+        @triangle[row_num] = calculate_next_row(previous_row)
     end
 
-    def calculate(old_row)
+    def calculate_next_row(previous_row)
         middle = []
-        (old_row.length - 1).times do |i|
-            middle.push(old_row[i] + old_row[i + 1])
+        (previous_row.length - 1).times do |i|
+            middle.push(previous_row[i] + previous_row[i + 1])
         end
         rows = [1, middle, 1].flatten
-        rows
     end
 
     def draw(input)
-        (input).times { |rows| p PascalsTriangle.new.to_s(rows) }
+        input.times { |rows| p PascalsTriangle.new.to_s(rows) }
     end
 
     def to_s(row_num)
-        triangle_row(row_num).join(" ").center(40)
+        triangle_row(row_num).join(" ").center(ALIGN_CENTER)
     end
 end
